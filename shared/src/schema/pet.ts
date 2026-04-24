1058daf0586bfc87ffd33a78c69f236b2953b010
S L <shengtaolicoding@gmail.com>
2026-04-24 07:23:14 -0700

    more installation fix

--- shared/src/schema/pet.ts
+++ shared/src/schema/pet.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
-import { AnimalType } from "../generated/prisma/enums";
+enum  AnimalType {
+  DOG = 'DOG',
+  CAT = 'CAT',
+  BIRD = 'BIRD',
+  RABBIT = 'RABBIT',
+  OTHER = 'OTHER'
+}
 
 export const ANIMAL_TYPES = Object.values(AnimalType);
 const AnimalTypeZ = z.enum(AnimalType);
